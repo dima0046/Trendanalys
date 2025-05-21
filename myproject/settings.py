5bddf708a46d526f9bfd0891e0e7815b42ded252
D. Furaev <dima0046@gmail.com>
2025-05-21 18:12:37 +0300

    add 'temp_data' to settings

--- myproject/settings.py
+++ myproject/settings.py
@@ -70,6 +70,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'myapp', 'static'),
+    os.path.join(BASE_DIR, 'myapp', 'temp_data'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
