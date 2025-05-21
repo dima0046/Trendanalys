bfbd91061894120c95d3b8d527fffa165220772c
D. Furaev <dima0046@gmail.com>
2025-05-21 15:57:44 +0300

    whitenoise installing

--- myproject/settings.py
+++ myproject/settings.py
@@ -26,6 +26,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
 ]
 
 ROOT_URLCONF = 'myproject.urls'
